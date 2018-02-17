0a6a8330628e3bb3742137c32da06e5a45db7715
Luis Cortes <lacortes@cpp.edu>
2018-02-16 19:49:29 -0800

    Queen class to hold queen location

---
+++ Project_2/NQueen/src/cortes/luis/Queen.java
@@ -0,0 +1,20 @@
+package cortes.luis;
+
+public class Queen {
+    private int row;
+    private int col;
+
+    public Queen(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+}
